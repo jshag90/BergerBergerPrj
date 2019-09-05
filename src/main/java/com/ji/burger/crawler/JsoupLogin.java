75e5587981485569a82a0a0e8b136fc679994fa5
jiseonhak <jshag90@naver.com>
2019-09-05 22:25:40 +0900

    190905 - 버거킹 홈페이지 셀레니움 라이브러리 사용해서 자동으로 아이디 비번 클릭 기능 구현

---
+++ src/main/java/com/ji/burger/crawler/JsoupLogin.java
@@ -0,0 +1,45 @@
+package com.ji.burger.crawler;
+
+import java.io.IOException;
+
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class JsoupLogin {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+				 try
+			        {
+			            Response initialResponse;
+
+			            initialResponse = Jsoup.connect("https://www.burgerking.co.kr/#/login")
+			                    .execute();
+
+			            Response loginResponse = Jsoup
+			                    .connect("https://www.burgerking.co.kr/#/login")
+			                    .cookies(initialResponse.cookies())
+			                    .data("UserName", "jshag90")
+			                    .data("Password", "goffhdn90#")
+			                    .data("IsRememberMe", "false")
+			                                    .method(Method.POST)
+			                    .execute();
+			                    //example of internal moody's page.
+			            Document doc = Jsoup.connect("https://www.burgerking.co.kr/#/deliveryHome")
+			                    .cookies(loginResponse.cookies()).timeout(3000000).get();
+
+			                    //simple div selection example
+			            System.out.println(doc);
+			        }
+			        catch (IOException e)
+			        {
+			            // TODO Auto-generated catch block
+			            e.printStackTrace();
+			        }
+
+	}
+
+}
