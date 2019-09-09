928ba6e5f50e6cf3b9cd4b9929a9eadc293ea568
jiseonhak <jshag90@naver.com>
2019-09-09 23:11:16 +0900

    스페셜 햄버거 이름, 가격 크롤링 완료

--- src/main/java/com/ji/burger/crawler/SeleniumTest2.java
+++ src/main/java/com/ji/burger/crawler/SeleniumTest2.java
@@ -15,7 +15,6 @@ public class SeleniumTest2 {
 
 		SeleniumTest2 selTest = new SeleniumTest2();
 		selTest.crawl();
-		
 
 	}
 
@@ -24,7 +23,6 @@ public class SeleniumTest2 {
 
 	private List<WebElement> webElement;
 	private List<WebElement> webElement2;
-	private List<WebElement> webElement3;
 
 	// Properties
 	public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
@@ -34,22 +32,20 @@ public class SeleniumTest2 {
 	private String base_url, base_url2;
 
 	public SeleniumTest2() {
-	        super();
-	 
-	        //System Property SetUp
-	        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
-	        
-	                
-	        //Driver SetUp
-	         ChromeOptions options = new ChromeOptions();
-	         options.setCapability("ignoreProtectedModeSettings", true);
-	         driver = new ChromeDriver(options);
-	        
-	        base_url = "https://www.burgerking.co.kr/#/login";
-	        
-	        
-	        base_url2 = "https://www.burgerking.co.kr/#/deliveryHome";
-	    }
+		super();
+
+		// System Property SetUp
+		System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+
+		// Driver SetUp
+		ChromeOptions options = new ChromeOptions();
+		options.setCapability("ignoreProtectedModeSettings", true);
+		driver = new ChromeDriver(options);
+
+		base_url = "https://www.burgerking.co.kr/#/login";
+
+		base_url2 = "https://www.burgerking.co.kr/#/deliveryHome";
+	}
 
 	public void crawl() {
 
@@ -58,33 +54,20 @@ public class SeleniumTest2 {
 			driver.get(base_url);
 
 			webElement = driver.findElements(By.tagName("input"));
-			
-			 String burgerKingId = "jshag90@naver.com";
-			 webElement.get(0).sendKeys(burgerKingId);
-			 Thread.sleep(1000);
-			
-			String burgerKingPw = "####";
+
+			String burgerKingId = "jshag90@naver.com";
+			webElement.get(0).sendKeys(burgerKingId);
+			Thread.sleep(1000);
+
+			String burgerKingPw = "######";
 			webElement.get(1).sendKeys(burgerKingPw);
 			Thread.sleep(1000);
-			
-			 List<WebElement> spans = driver.findElements(By.tagName("span"));
-		     Iterator<WebElement> i = spans.iterator();
-		     
-			int index = 0;
-			while (i.hasNext()) {
-				WebElement span = i.next();
-				index++;
-				if (index == 39) {
-					Thread.sleep(1000);
-					span.click();
-					break;
-				}
-			}
-		        
-		    Thread.sleep(3000);
-		    
-		    driver.get(base_url2);
-		    Thread.sleep(3000);
+
+			WebElement webElement = driver.findElement(By.className("btn01"));
+			webElement.click();
+
+			driver.get(base_url2);
+			Thread.sleep(3000);
 			webElement2 = driver.findElements(By.className("tit"));
 			Iterator<WebElement> ps = webElement2.iterator();
 
@@ -92,21 +75,14 @@ public class SeleniumTest2 {
 				WebElement pTag = ps.next();
 				System.out.println(pTag.getText());
 			}
-			
-			webElement3 =  driver.findElements(By.className("price"));
-			Iterator<WebElement> spanTags = webElement3.iterator();
-			
-			while (spanTags.hasNext()) {
-				WebElement span = spanTags.next();
 
-				System.out.println(span.getText());
+			List<WebElement> webElement3 = driver.findElements(By.className("price"));
+
+			for (WebElement el : webElement3) {
+				System.out.println(el.findElement(By.xpath("//strong//em//span")).getText());
 			}
-			
-//			driver.get("https://www.burgerking.co.kr/#/deliveryHome");
-//
-//			System.out.println(driver.getPageSource());
 
-
+			// System.out.println(driver.getPageSource());
 
 		} catch (Exception e) {
 
