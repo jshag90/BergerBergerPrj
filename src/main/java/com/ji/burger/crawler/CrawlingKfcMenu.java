deb9e8d4ef22166b50f680a32d5836023da181d4
jiseonhak <jshag90@naver.com>
2019-09-26 22:29:53 +0900

    kfc 메뉴 클릭 구현

---
+++ src/main/java/com/ji/burger/crawler/CrawlingKfcMenu.java
@@ -0,0 +1,119 @@
+package com.ji.burger.crawler;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class CrawlingKfcMenu {
+
+	public static void main(String[] args) {
+
+		CrawlingKfcMenu selTest = new CrawlingKfcMenu();
+		selTest.crawl();
+
+	}
+
+	// WebDriver
+	private WebDriver driver;
+
+	private List<WebElement> webElement;
+
+	// Properties
+	public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
+	public static final String WEB_DRIVER_PATH = "C:/selenium/chromedriver.exe";
+
+	// 크롤링 할 URL
+	private String base_url, base_url2;
+
+	public CrawlingKfcMenu() {
+		super();
+
+		// System Property SetUp
+		System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+
+		// Driver SetUp
+		ChromeOptions options = new ChromeOptions();
+		options.setCapability("ignoreProtectedModeSettings", true);
+		driver = new ChromeDriver(options);
+
+		base_url = "https://www.kfckorea.com";
+
+		base_url2 = "https://www.mcdelivery.co.kr/kr/menu.html";
+	}
+
+	public void crawl() {
+
+		try {
+			// get page (= 브라우저에서 url을 주소창에 넣은 후 request 한 것과 같다)
+			driver.get(base_url);
+
+			WebElement category= driver.findElement(By.className("category"));
+			WebElement categoryList = category.findElement(By.className("category-list"));
+			List<WebElement> categoryMenu = categoryList.findElements(By.xpath("//li//a"));
+			Thread.sleep(1000);
+			for(WebElement menu: categoryMenu) {
+//				System.out.println(menu.getText());
+				if(menu.getText().equals("DELIVERY")) {
+					menu.click();
+					break;
+				}
+			}
+			
+			Thread.sleep(1000);
+			for(WebElement menu: categoryMenu) {
+//				System.out.println(menu.getText());
+				if(menu.getText().equals("치킨&세트")) {
+					menu.click();
+					break;
+				}
+			}
+			
+			Thread.sleep(5000);
+			
+			
+			
+//			WebElement menuEl = category.get(1);
+//			menuEl.findElement(By.tagName("a")).click();
+			
+			
+//			driver.get(base_url2);
+			
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			driver.close();
+		}
+
+	}
+
+	public void readProductionMenuInfo() {
+		
+		List<WebElement> prductTitle =  driver.findElements(By.className("product-title"));
+		List<WebElement> prductPrice =  driver.findElements(By.className("starting-price"));
+		
+		for(int index = 0; index<prductTitle.size(); index++) {
+			System.out.println(prductTitle.get(index).getText());
+			String priceStr = prductPrice.get(index).getText().trim().replace(" ", "");
+			System.out.println(priceStr.replace("₩", ""));
+		}
+
+	}
+
+	public void clickMenu(int index) {
+		WebElement webElementSecondaryMenu= driver.findElement(By.className("secondary-menu"));
+		List<WebElement> webMenuList = webElementSecondaryMenu.findElements(By.tagName("li"));
+		WebElement menuEl = webMenuList.get(index);
+		menuEl.findElement(By.className("secondary-menu-item-target")).click();
+
+	}
+
+}
